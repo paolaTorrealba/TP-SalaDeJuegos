758f820b0e56b864b69628dcccda18cb299fd980
Paola <torrealbapa@hotmail.com.ar>
2019-09-12 20:16:11 -0300

    segundo commit
    segundo commit

--- src/app/clases/juego-piedra-papel-tijera.ts
+++ src/app/clases/juego-piedra-papel-tijera.ts
@@ -1,2 +1,31 @@
-export class JuegoPiedraPapelTijera {
+import { Juego } from '../clases/juego'
+
+export class JuegoPiedraPapelTijera extends Juego {
+    numeroSecreto: number = 0;
+    numeroIngresado = 0;
+    constructor(nombre?: string, gano?: boolean, jugador?:string) {
+        super("",false,"");
+      
+    }
+    public verificar() {
+        if (this.numeroIngresado == this.numeroSecreto) {
+          this.gano = true;
+        }
+        if (this.gano) {
+          return true;
+        } else {
+          return false;
+        }
+     }
+     public generarnumero() {
+        this.numeroSecreto = Math.floor((Math.random() * 100) + 1);
+        console.info('numero Secreto:' + this.numeroSecreto);
+        this.gano = false;
+      }
+      public retornarAyuda() {
+        if (this.numeroIngresado < this.numeroSecreto) {
+          return "Falta";
+        }
+        return "Te pasate";
+      }
 }
--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {JuegoPiedraPapelTijera} from '../../clases/juego-piedra-papel-tijera'
 
 @Component({
   selector: 'app-piedra-papel-tijera',
@@ -6,9 +7,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./piedra-papel-tijera.component.css']
 })
 export class PiedraPapelTijeraComponent implements OnInit {
+  @Output() enviarJuego: EventEmitter<any>= new EventEmitter<any>();
 
-  constructor() { }
+  nuevoJuego: JuegoPiedraPapelTijera;
+  Mensajes:string;
+  contador:number;
+  ocultarVerificar:boolean;
+ 
+  constructor() { 
+    this.nuevoJuego = new JuegoPiedraPapelTijera();
+    console.info("numero Secreto:",this.nuevoJuego.numeroSecreto);  
+    this.ocultarVerificar=false;
+  }
+  generarnumero() {
+    this.nuevoJuego.generarnumero();
+    this.contador=0;
+  }
+  verificar()
+  {
+    this.contador++;
+    this.ocultarVerificar=true;
+    console.info("numero Secreto:",this.nuevoJuego.gano);  
+    if (this.nuevoJuego.verificar()){
+      
+      this.enviarJuego.emit(this.nuevoJuego);
+      this.MostarMensaje("Sos un Genio!!!",true);
+      this.nuevoJuego.numeroSecreto=0;
 
+    }else{
+
+      let mensaje:string;
+      switch (this.contador) {
+        case 1:
+          mensaje="No, intento fallido, animo";
+          break;
+          case 2:
+          mensaje="No,Te estaras Acercando???";
+          break;
+          case 3:
+          mensaje="No es, Yo crei que la tercera era la vencida.";
+          break;
+          case 4:
+          mensaje="No era el  "+this.nuevoJuego.numeroIngresado;
+          break;
+          case 5:
+          mensaje=" intentos y nada.";
+          break;
+          case 6:
+          mensaje="Afortunado en el amor";
+          break;
+      
+        default:
+            mensaje="Ya le erraste "+ this.contador+" veces";
+          break;
+      }
+      this.MostarMensaje("#"+this.contador+" "+mensaje+" ayuda :"+this.nuevoJuego.retornarAyuda());
+     
+
+    }
+    console.info("numero Secreto:",this.nuevoJuego.gano);  
+  }  
+
+  MostarMensaje(mensaje:string="este es el mensaje",ganador:boolean=false) {
+    this.Mensajes=mensaje;    
+    var x = document.getElementById("snackbar");
+    if(ganador)
+      {
+        x.className = "show Ganador";
+      }else{
+        x.className = "show Perdedor";
+      }
+    var modelo=this;
+    setTimeout(function(){ 
+      x.className = x.className.replace("show", "");
+      modelo.ocultarVerificar=false;
+     }, 3000);
+    console.info("objeto",x);
+  
+   }  
   ngOnInit() {
   }
 
